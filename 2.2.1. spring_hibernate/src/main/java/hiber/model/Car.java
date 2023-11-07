a3327d08b6455f4366b1b96d24c453dfa8b4ec7e
Руслан Шевченко <r.schewtschenko@mail.ru>
2023-11-08 01:32:01 +0700

    Put back the one-to-one annotation to the Car class.

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -23,8 +23,8 @@ public class Car {
     @Column(name = "series")
     private int series;
 
-//    @OneToOne(cascade = CascadeType.ALL)
-//    @JoinColumn(name = "user_id")
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
     private User user;
 
     public Car() {}

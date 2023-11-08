46c8956f820100ae23fe80479178842a64eb663b
Ruslan <r.schewtschenko@mail.ru>
2023-11-08 07:19:09 +0300

    Rebasing the correct version (which was deleted by an older version from home computer) from other client on the working computer. Also replaced all wild card imports.

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import java.sql.SQLException;
 import java.util.List;
 
+
 public class MainApp {
    public static void main(String[] args) throws SQLException {
       AnnotationConfigApplicationContext context = 
